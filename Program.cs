6fbb84480106b52871515c02e828e30eff725f6b
Selda Larsson <selda.larssonsvensdotter@elev.ga.ntig.se>
2023-08-31 10:27:02 +0200

    Första commiten

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿Console.ForegroundColor = ConsoleColor.Blue;
+
+Console.WriteLine("Bonjour");
+
+string Name1 = "Zelda";
+String Name2 = "Larsson";
+
+Console.WriteLine($"Hej {Name1} {Name2},Vad snygg du är idag!");
+
+Console.ReadLine();
+
+Console.WriteLine("Hur gammal är du?");
+
+string age = Console.ReadLine ();
+
+Console.WriteLine($"Det är sjukt att du har överleft i {age} år!!!");
+
+Console.ReadLine();
